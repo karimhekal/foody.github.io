92b851272781f35af73f8a2d3a20c03a8d521fed
Karim Hekal <43865216+karimhekal@users.noreply.github.com>
2022-02-11 02:46:09 +0200

    Update firebase.js

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -3,13 +3,13 @@ import * as firebase from 'firebase/app'
 import { getStorage } from 'firebase/storage';  // <----
 import { getFirestore, collection, getDocs } from 'firebase/firestore';
 export const firebaseConfig = {
-    apiKey: "AIzaSyAL78UGSHR5MDMSmxFG2teA5Xdo2_jk5Rs",
-    authDomain: "authentication-50462.firebaseapp.com",
-    databaseURL: "https://authentication-50462.firebaseio.com",
-    projectId: "authentication-50462",
-    storageBucket: "authentication-50462.appspot.com",
-    messagingSenderId: "415779728307",
-    appId: "1:415779728307:web:e3dec1e905849065"
+    apiKey: "API here",
+    authDomain: ".....",
+    databaseURL: "...",
+    projectId: ".....",
+    storageBucket: ".....",
+    messagingSenderId: "....",
+    appId: "........."
 };
 
 export const app = firebase.initializeApp(firebaseConfig);
